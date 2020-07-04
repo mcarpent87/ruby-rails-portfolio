15d95c80191b90332e5bcc5fd452f0dde767d2a4
McLean Carpenter <carpemc@McLeans-Mac-mini.local>
2020-07-04 11:06:24 -0600

    created comment broadcast, configured websocket route

--- app/jobs/comment_broadcast_job.rb
+++ app/jobs/comment_broadcast_job.rb
@@ -1,13 +1,13 @@
 class CommentBroadcastJob < ApplicationJob
-  queue_as :default
-
-  def perform(comment)
-    ActionCable.server.broadcast "blogs_#{comment.blog.id}_channel", comment: render_comment(comment)
-  end
-
-  private
-
-  def render_comment(comment)
-    CommentController.render partial: 'comments/comment', locals: { comment: comment }
-  end
+    queue_as :default
+  
+    def perform(comment)
+      ActionCable.server.broadcast "blogs_#{comment.blog.id}_channel", comment: render_comment(comment)
+    end
+  
+    private
+  
+    def render_comment(comment)
+      CommentsController.render partial: 'comments/comment', locals: { comment: comment }
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-  devise_for :installs
   devise_for :users, path: '', path_names: { sign_in: 'login', sign_out: 'logout', sign_up: 'register' }
   resources :portfolios, except: [:show] do
     put :sort, on: :collection
@@ -9,13 +8,14 @@ Rails.application.routes.draw do
 
   get 'about-me', to: 'pages#about'
   get 'contact', to: 'pages#contact'
+  get 'tech-news', to: 'pages#tech_news'
 
   resources :blogs do
     member do
       get :toggle_status
     end
   end
-  
+
   mount ActionCable.server => '/cable'
 
   root to: 'pages#home'
